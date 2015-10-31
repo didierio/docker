8f3ce2835f1e38f6c03408791bd7cc7f524bef56
Julien DIDIER <julien@didier.io>
2015-10-31 13:10:40 +0100

    clean dockerization

--- init.sh
+++ init.sh
@@ -5,39 +5,31 @@ set -e
 green='\033[1;32m'
 yellow='\033[1;33m'
 NC='\033[0m' # No Color
-echo "${green}fetching repositories${NC}"
 
-if [ ! -d "blog" ]; then
+echo "${green}fetching repositories...${NC}"
+
+if [ ! -d "./apps/blog" ]; then
     echo "${yellow}cloning blog...${NC}"
 
-    git clone git@github.com:didierio/blog.git --quiet
+    git clone git@github.com:didierio/blog.git ./apps/blog --quiet
 fi
 
-if [ ! -d "cache" ]; then
+if [ ! -d "./apps/cache" ]; then
     echo "${yellow}cloning cache...${NC}"
 
-    git clone git@github.com:didierio/cache.git --quiet
+    git clone git@github.com:didierio/cache.git ./apps/cache --quiet
 fi
 
-if [ ! -d "connect" ]; then
+if [ ! -d "./apps/connect" ]; then
     echo "${yellow}cloning connect...${NC}"
 
-    git clone git@github.com:didierio/connect.git --quiet
+    git clone git@github.com:didierio/connect.git ./apps/connect --quiet
 fi
 
-if [ ! -d "website" ]; then
+if [ ! -d "./apps/website" ]; then
     echo "${yellow}cloning website...${NC}"
 
-    git clone git@github.com:didierio/website.git --quiet
+    git clone git@github.com:didierio/website.git ./apps/website --quiet
 fi
 
-if [ ! -d "work" ]; then
-    echo "${yellow}cloning work...${NC}"
-
-    git clone git@github.com:didierio/work.git --quiet
-fi
-
-
-
-
-
+echo "${green}done!${NC}"
